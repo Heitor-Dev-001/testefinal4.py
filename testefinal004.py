a08036b13dee8332afe76a3ff8d474ebe1aa22e3
Heitor Silva <heitorsilvanascimento@hotmail.com>
2024-11-12 14:25:43 -0300

    Add files via upload

---
+++ testefinal004.py
@@ -0,0 +1,4 @@
+#Para o Python, os valores verdadeiro e falso 
+# são do tipo ____Bool____ e são representados pelos valores ____True____ e __False______. 
+# Qual das opções a seguir é a única que contém as palavras 
+# que completam as lacunas da frase anterior, na ordem?
